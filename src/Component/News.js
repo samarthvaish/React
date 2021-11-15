ed4f6b83e38d8942e97bec274a6ed4af09c33fc6
samarthvaish <vaishsamarth@gmail.com>
2021-11-15 15:18:30 +0530

    added .env.local file

--- src/Component/News.js
+++ src/Component/News.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import { Spinner } from "./Spinner";
 import PropTypes from "prop-types";
-require("dotenv").config();
 export class News extends Component {
   newsApiKey = process.env.REACT_APP_NewsApiKey;
   articles = [
